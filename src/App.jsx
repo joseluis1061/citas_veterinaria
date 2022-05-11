c2570d350dbf739a80fb9659dbe1979f9b102344
jose <joseluis836@hotmail.com>
2022-05-11 13:41:59 -0500

    Se agrego el titulo y la sección de seguimiento de pacientes. Con estilos para cada sección usando tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import Header from "./components/Header"
+import Formulario from "./components/Formulario"
+import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
+    <div className="container mx-auto mt-20">
+      <Header />
+      <div className="mt-12 md:flex">
+        <Formulario />
+        <ListadoPacientes/>
+      </div>
     </div>
   )
 }
